c30c2423c1c880d893f48cf0a942747f913e712b
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-08-13 23:04:30 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,30 @@
+#function - A block of reusable code
+# Place () after the function name to invoke it
+
+def happy_birthday(name,age):
+  print(f"Happy birthday to {name}!")
+  print(f"You are {age} years old")
+  print("Happy birthday to you!")
+  print()
+  
+happy_birthday("Sajin",21)
+happy_birthday("Lijin",20)
+happy_birthday("Aaron",22)
+
+def mes(name):
+  print(f"We Welcome you {name} role")
+mes("SAJIN")
+
+# - O/P - We Welcome you SAJIN
+
+def hello(a,b):
+  print(f"Hello there{a}")
+  print(f"You've been selected for {b} role at Google")
+  print()
+hello(" Sajin","DataAnalysis")
+
+#O/P 
+'''
+Hello there Sajin
+You've been selected for DataAnalysis role at Google
+'''
